71f774a97b0f80893d18ef7e8edd5e7c8ec22629
Carlo J <422247668@qq.com>
2017-05-30 11:15:57 +0800

    update adapter pattern code

--- Pattern/Adaptee.php
+++ Pattern/Adaptee.php
@@ -24,7 +24,7 @@ interface Target
     }
 }
 
-class MySQLAdaptee implements Target
+class MySQLAdapter implements Target
 {
     protected $conn;
     public function connect($host, $user, $passwd, $dbname)
@@ -49,3 +49,65 @@ class MySQLAdaptee implements Target
     }
 }
 
+class MySQLiAdapter implements Target
+{
+    protected $conn;
+    public function connect($host, $user, $passwd, $dbname)
+    {
+        // TODO: Implement connect() method.
+        $conn = mysqli_connect($host, $user, $passwd);
+        mysqli_select_db($dbname, $conn);
+        $this->conn = $conn;
+    }
+
+    public function query($sql)
+    {
+        // TODO: Implement query() method.
+        $result = mysqli_query($sql, $this->conn);
+        return $result;
+    }
+
+    public function close()
+    {
+        // TODO: Implement close() method.
+        mysqli_close($this->conn);
+    }
+}
+
+class DataBase implements Target
+{
+    protected $db;
+    public function __construct($type)
+    {
+        $type = $type.'Adapter';
+        $this->db = new $type;
+    }
+
+    public function connect($host, $user, $passwd, $dbname)
+    {
+        // TODO: Implement connect() method.
+        $this->db->connect($host, $user, $passwd, $dbname);
+    }
+
+    public function query($sql)
+    {
+        // TODO: Implement query() method.
+        return $this->db->query($sql);
+    }
+
+    public function close()
+    {
+        // TODO: Implement close() method.
+        $this->db->close();
+    }
+}
+
+$db1 = new DataBase('MySQL');
+$db1->connect('127.0.0.1', 'root','root','myDB');
+$db1->query('select * from test');
+$db1->close();
+
+$db2 = new DataBase('MySQLi');
+$db2->connect('127.0.0.1', 'root','root','myDB');
+$db2->query('select * from test');
+$db2->close();
